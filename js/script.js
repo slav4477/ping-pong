421904592c8a5a20910a0a57646eab162192038d
Slavik Mygovych <slavik4477@gmail.com>
2016-10-09 22:47:21 -0700

    Apply JavaScript functions for the app to return correct results.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@ function pingPong(userNumber) {
   if(userNumber <= 0) {
     alert("Please enter a number that is greater than 0.");
   } else if(userNumber >= 1 && userNumber <= 1000) {
-
+    outputArray = [];
+    for(i = 1; i <= userNumber; i++){
+      if(i % 15 === 0) {
+        outputArray.push("<li>Ping Pong</li>");
+      } else if(i % 5 === 0) {
+        outputArray.push("<li>Pong</li>");
+      } else if(i % 3 === 0) {
+        outputArray.push("<li>Ping</li>");
+      } else{
+        outputArray.push("<li>" + i + "</li>");
+      }
+    }
+    return outputArray;
   } else {
     alert("Please enter a number that is 1000 or less.");
   }
